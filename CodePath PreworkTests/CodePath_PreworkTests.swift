8e2fba27a156af38d7efdd09f6516b7e74144891
respinoza13 <raespinoza1333@gmail.com>
2025-09-01 21:33:37 -0400

    Added pre-work files to repository

---
+++ CodePath PreworkTests/CodePath_PreworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodePath_PreworkTests.swift
+//  CodePath PreworkTests
+//
+//  Created by Reynaldo Amaro Espinoza on 9/1/25.
+//
+
+import Testing
+@testable import CodePath_Prework
+
+struct CodePath_PreworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ CodePath PreworkUITests/CodePath_PreworkUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CodePath_PreworkUITestsLaunchTests.swift
+//  CodePath PreworkUITests
+//
+//  Created by Reynaldo Amaro Espinoza on 9/1/25.
+//
+
+import XCTest
+
+final class CodePath_PreworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
